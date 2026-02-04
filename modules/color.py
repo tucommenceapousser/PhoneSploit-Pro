d0a12d9fc935952d3f811b400a1b33c503350536
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2026-02-04 04:44:55 +0100

    Update color.py

--- modules/color.py
+++ modules/color.py
@@ -27,15 +27,15 @@
 
 RED = "\033[91m"
 GREEN = "\033[92m"
-# ORANGE = '\033[33m'
+ORANGE = '\033[33m'
 YELLOW = "\033[93m"
-# BLUE = '\033[94m'
+BLUE = '\033[94m'
 PURPLE = "\033[95m"
 CYAN = "\033[96m"
 WHITE = "\033[97m"
 
 
-color_list = [RED, GREEN, YELLOW, PURPLE, CYAN, WHITE]
+color_list = [RED, GREEN, YELLOW, PURPLE, CYAN, WHITE, ORANGE, BLUE]
 
 """
 Copyright © 2023 Mohd Azeem (github.com/AzeemIdrisi)
